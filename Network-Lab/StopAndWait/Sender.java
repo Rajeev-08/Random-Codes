9f2ddc9f85622f96d70748992486a357d8158aa7
Rajeev <147076514+Rajeev-08@users.noreply.github.com>
2025-02-25 22:09:53 +0530

    Update Sender.java

--- Network-Lab/StopAndWait/Sender.java
+++ Network-Lab/StopAndWait/Sender.java
@@ -1,82 +1,40 @@
-
 import java.io.*;
-
 import java.net.*;
-
 import java.util.Scanner;
 
-class Sender
+class Sender {
+    public static void main(String args[]) throws Exception {
+        Sender sws = new Sender();
+        sws.run();
+    }
 
-{
+    public void run() throws Exception {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of frames to be sent:");
+        int n = sc.nextInt();
 
-public static void main(String args[]) throws Exception
+        Socket myskt = new Socket("localhost", 9999);
+        PrintStream myps = new PrintStream(myskt.getOutputStream());
 
-{
+        for (int i = 0; i <= n;) {
+            if (i == n) {
+                myps.println("exit");
+                break;
+            }
 
-Sender sws = new Sender();
+            System.out.println("Frame no " + i + " is sent");
+            myps.println(i);
 
-sws.run();
+            BufferedReader bf = new BufferedReader(new InputStreamReader(myskt.getInputStream()));
+            String ack = bf.readLine();
 
-}
-
-public void run() throws Exception
-
-{
-
-Scanner sc=new Scanner(System.in);
-
-System.out.println("Enter no of frames to be sent:");
-
-int n=sc.nextInt();
-
-Socket myskt=new Socket("localhost",9999);
-
-PrintStream myps=new PrintStream(myskt.getOutputStream());
-
-for(int i=0;i<=n;)
-
-{
-
-if(i==n)
-
-{
-
-myps.println("exit");
-
-break;
-
-}
-
-System.out.println("Frame no "+i+" is sent");
-
-myps.println(i);
-
-BufferedReader bf=new BufferedReader(new InputStreamReader(myskt.getInputStream()));
-
-String ack=bf.readLine();
-
-if(ack!=null)
-
-{
-
-System.out.println("Acknowledgement was Received from receiver");
-
-i++;
-
-Thread.sleep(4000);
-
-}
-
-else
-
-{
-
-myps.println(i);
-
-}
-
-}
-
-}
-
+            if (ack != null) {
+                System.out.println("Acknowledgement was received from receiver");
+                i++;
+                Thread.sleep(4000);
+            } else {
+                myps.println(i);
+            }
+        }
+    }
 }
