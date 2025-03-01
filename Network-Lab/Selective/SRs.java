8eddcba4218c3d89e82ca6ea89f9af554cfda268
Rajeev <147076514+Rajeev-08@users.noreply.github.com>
2025-03-01 17:01:59 +0530

    Update SRs.java

--- Network-Lab/Selective/SRs.java
+++ Network-Lab/Selective/SRs.java
@@ -1,46 +1,27 @@
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.net.SocketException;
- 
-public class SRs {
-    static ServerSocket Serversocket;
-    static DataInputStream dis;
-    static DataOutputStream dos;
- 
-    public static void main(String[] args) throws SocketException {
-        try {
-            int a[] = { 30, 40, 50, 60, 70, 80, 90, 100 };
-            Serversocket = new ServerSocket(8011);
-            System.out.println("waiting for connection");
-            Socket client = Serversocket.accept();
-            dis = new DataInputStream(client.getInputStream());
-            dos = new DataOutputStream(client.getOutputStream());
-            System.out.println("The number of packets sent is:" + a.length);
-            int y = a.length;
-            dos.write(y);
-            dos.flush();
-            for (int i = 0; i < a.length; i++) {
-                dos.write(a[i]);
-                dos.flush();
-            }
-            int k = dis.read();
-            dos.write(a[k]);
+import java.io.*;
+import java.net.*;
+public class Server{
+    public static void main(String[]args){
+        try{
+        int a[]={30,40,50,60,70,80,90,100};
+        ServerSocket server=new ServerSocket(8011);
+        System.out.println("waiting for connection..");
+        Socket client=server.accept();
+        DataInputStream dis=new DataInputStream(client.getInputStream());
+        DataOutputStream dos=new DataOutputStream(client.getOutputStream());
+        System.out.println("the number of packet sent is :"+a.length);
+        int y=a.length;
+        dos.write(y);
+        dos.flush();
+        for (int i=0;i<y;i++){
+            dos.write(a[i]);
             dos.flush();
         }
-        catch (IOException e){
-            System.out.println(e);
-        }
-        finally{
-            try{
-                dis.close();
-                dos.close();
-            }
-            catch (IOException e){
-                e.printStackTrace();
-            }
-        }
-    }
+        int k=dis.read();
+        dos.write(a[k]);
+        dos.flush();
+
+
+    }catch(Exception e){}
+}
 }
