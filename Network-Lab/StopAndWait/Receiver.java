ba1c460d5ac7b492f202615ba33c1bce4adea88a
Rajeev <147076514+Rajeev-08@users.noreply.github.com>
2025-03-23 11:22:56 +0530

    Update Receiver.java

--- Network-Lab/StopAndWait/Receiver.java
+++ Network-Lab/StopAndWait/Receiver.java
@@ -1,34 +1,30 @@
 import java.io.*;
 import java.net.*;
 
-class Receiver {
-    public static void main(String args[]) throws Exception {
-        Receiver swr = new Receiver();
-        swr.run();
+public class Receiver{
+    public static void main(String[]args) throws Exception{
+        Receiver r= new Receiver();
+        r.run();
     }
+    public void run()throws Exception{
+        ServerSocket server= new ServerSocket(5555);
+        Socket client=server.accept();
 
-    public void run() throws Exception {
-        String temp = "any message", str = "exit";
+        String temp="abc";
+        BufferedReader br=new BufferedReader(new InputStreamReader(client.getInputStream()));
+        PrintStream p=new PrintStream(client.getOutputStream());
 
-        ServerSocket myss = new ServerSocket(9999);
-        Socket ss_accept = myss.accept();
-
-        BufferedReader ss_bf = new BufferedReader(new InputStreamReader(ss_accept.getInputStream()));
-        PrintStream myps = new PrintStream(ss_accept.getOutputStream());
-
-        while (temp.compareTo(str) != 0) {
+        while(temp.compareTo("exit")!=0){
             Thread.sleep(1000);
-            temp = ss_bf.readLine();
-
-            if (temp.compareTo(str) == 0) {
+            temp=br.readLine();
+            if(temp.compareTo("exit")==0){
                 break;
             }
 
-            System.out.println("Frame " + temp + " was received");
+            System.out.println("Frame "+temp+" was received");
             Thread.sleep(500);
-            myps.println("Received");
+            p.println("received");
         }
-
-        System.out.println("ALL FRAMES WERE RECEIVED SUCCESSFULLY");
-    }
+        System.out.println("ALL FRAMES RECEIVED");
+    } 
 }
