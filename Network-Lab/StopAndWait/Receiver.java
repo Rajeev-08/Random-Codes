d69ce3b2044db05b628b8b2becd1b532bbeaf690
Rajeev <147076514+Rajeev-08@users.noreply.github.com>
2025-04-09 13:56:47 +0530

    Update Receiver.java

--- Network-Lab/StopAndWait/Receiver.java
+++ Network-Lab/StopAndWait/Receiver.java
@@ -1,30 +1,29 @@
 import java.io.*;
 import java.net.*;
 
-public class Receiver{
-    public static void main(String[]args) throws Exception{
-        Receiver r= new Receiver();
-        r.run();
-    }
-    public void run()throws Exception{
-        ServerSocket server= new ServerSocket(5555);
-        Socket client=server.accept();
+public class R{
+    public static void main(String[]args)throws Exception{
 
-        String temp="abc";
+        ServerSocket server=new ServerSocket(5555);
+        System.out.println("Waiting For connection...");
+        Socket client=server.accept();
         BufferedReader br=new BufferedReader(new InputStreamReader(client.getInputStream()));
         PrintStream p=new PrintStream(client.getOutputStream());
+        String temp="";
 
         while(temp.compareTo("exit")!=0){
             Thread.sleep(1000);
             temp=br.readLine();
-            if(temp.compareTo("exit")==0){
-                break;
-            }
 
-            System.out.println("Frame "+temp+" was received");
+            if(temp.compareTo("exit")==0)
+                break;
+
+            System.out.println("Frame "+temp+" is received");
             Thread.sleep(500);
-            p.println("received");
+            p.println("Ack");
+
         }
-        System.out.println("ALL FRAMES RECEIVED");
-    } 
+        
+        System.out.println("ALL FRAMES RECEIVED SUCCESSFULLY");
+    }
 }
