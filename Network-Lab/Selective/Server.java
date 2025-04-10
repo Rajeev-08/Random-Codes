b32f0d6f065da820c5f7fc35c5c801a3cb4dd80e
Rajeev <147076514+Rajeev-08@users.noreply.github.com>
2025-04-10 07:13:25 +0530

    Update Server.java

--- Network-Lab/Selective/Server.java
+++ Network-Lab/Selective/Server.java
@@ -3,12 +3,12 @@ import java.net.*;
 public class Server{
     public static void main(String[]args) throws Exception{
         int a[]={30,40,50,60,70,80,90,100};
+        int n=a.length;
         ServerSocket server=new ServerSocket(5555);
         System.out.println("Waiting for connection...");
         Socket client=server.accept();
         DataInputStream dis=new DataInputStream(client.getInputStream());
         DataOutputStream dos=new DataOutputStream(client.getOutputStream());
-        int n=a.length;
         System.out.println("No.of packets sent is : "+n);
         dos.write(n);
         dos.flush();
