15001e2097b579ba7b0ab34c1dc8aac645c93854
Rajeev <147076514+Rajeev-08@users.noreply.github.com>
2025-10-29 15:11:39 +0530

    Update First_Follow.c

--- Compiler-lab/First_Follow.c
+++ Compiler-lab/First_Follow.c
@@ -4,68 +4,62 @@
 
 #define MAX 10
 
-char prod[MAX][MAX];  
-int n;                // Number of productions
+int n;
+char prod[MAX][MAX];
+char first[20],follow[20];
 
-// Function declarations
-void findFirst(char, int, int);
-void findFollow(char);
-char first[100], follow[100];  // Increased size
-
-int isNonTerminal(char c) {
-    return (c >= 'A' && c <= 'Z');
+int isNontermianl(char c){
+    return (c>='A'&&c<='Z');
 }
 
-// ---------- FIND FIRST ----------
-void findFirst(char c, int q1, int q2) {
-    if (!isNonTerminal(c)) {  // Terminal
-        if (!strchr(first, c))
-            strncat(first, &c, 1);
-        return;
+void findfirst(char c){
+    if(!isNontermianl(c)){
+        if(!strchr(first,c))
+            strncat(first,&c,1);
     }
 
-    for (int i = 0; i < n; i++) {
-        if (prod[i][0] == c) {
-            if (prod[i][2] == '#') { // Epsilon
-                if (!strchr(first, '#'))
-                    strncat(first, "#", 1);
-            } else if (!isNonTerminal(prod[i][2])) {
-                if (!strchr(first, prod[i][2]))
-                    strncat(first, &prod[i][2], 1);
-            } else {
-                findFirst(prod[i][2], q1, q2);
+    for(int i=0;i<n;i++){
+        if(prod[i][0]==c){
+            if(prod[i][2]=='#'){
+                if(!strchr(first,'#'))
+                    strncat(first,"#",1);
+            }else if(!isNontermianl(prod[i][2])){
+                if(!strchr(first,prod[i][2]))
+                    strncat(first,&prod[i][2],1);
+            }else{
+                findfirst(prod[i][2]);
             }
         }
     }
 }
 
-// ---------- FIND FOLLOW ----------
-void findFollow(char c) {
-    if (prod[0][0] == c) {  // Start symbol
-        if (!strchr(follow, '$'))
-            strncat(follow, "$", 1);
+void findfollow(char c){
+    if(prod[0][0]==c){
+        if(!strchr(follow,'$'))
+            strncat(follow,"$",1);
     }
 
-    for (int i = 0; i < n; i++) {
-        for (int j = 2; j < strlen(prod[i]); j++) {
-            if (prod[i][j] == c) {
-                if (prod[i][j + 1] != '\0') {
-                    first[0] = '\0';  // Clear first before reuse
-                    findFirst(prod[i][j + 1], 0, 0);
-                    for (int k = 0; k < strlen(first); k++) {
-                        if (first[k] != '#' && !strchr(follow, first[k]))
-                            strncat(follow, &first[k], 1);
+    for(int i=0;i<n;i++){
+        for(int j=2;j<strlen(prod[i]);j++){
+            if(prod[i][j]==c){
+                if(prod[i][j+1]!='\0'){
+                    first[0]='\0';
+                    findfirst(prod[i][j+1]);
+                    for(int k=0;k<strlen(first);k++){
+                        if(first[k]!='#'&&!strchr(follow,first[k]))
+                            strncat(follow,&first[k],1);
                     }
-                    if (strchr(first, '#') && prod[i][0] != c)
-                        findFollow(prod[i][0]);
-                } else if (prod[i][0] != c)
-                    findFollow(prod[i][0]);
+                    if(strchr(first,'#')&&prod[i][0]!=c){
+                        findfollow(prod[i][0]);
+                    }
+
+                }else if(prod[i][0]!=c){
+                    findfollow(prod[i][0]);
+                }
             }
         }
     }
 }
-
-// ---------- MAIN ----------
 int main() {
     printf("Enter number of productions: ");
     scanf("%d", &n);
@@ -79,14 +73,14 @@ int main() {
     scanf(" %c", &c);
 
     first[0] = '\0';
-    findFirst(c, 0, 0);
+    findfirst(c);
     printf("FIRST(%c) = { ", c);
     for (int i = 0; i < strlen(first); i++)
         printf("%c ", first[i]);
     printf("}\n");
 
     follow[0] = '\0';
-    findFollow(c);
+    findfollow(c);
     printf("FOLLOW(%c) = { ", c);
     for (int i = 0; i < strlen(follow); i++)
         printf("%c ", follow[i]);
